ccd4751ddb509dd1ae98fa9585b5de00985e114c
Hélio Monteiro <heliojr.monteiro@yahoo.com.br>
2025-04-21 23:03:41 -0300

    inclusao de arquivos estaticos para o html do app

--- galeria/views.py
+++ galeria/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 def index(request):
 #    return HttpResponse('<h1>Alura Space</h1><p>Bem-vindo ao espaço</p>')
     return render(request, 'galeria\index.html')
+
+def imagem(request):
+    return render(request, 'galeria\imagem.html')
